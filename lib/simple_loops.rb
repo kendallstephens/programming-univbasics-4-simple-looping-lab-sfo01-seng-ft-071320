1b91dba56135524aec6c282d1829716808c09142
Kendall Stephens <kendoll.stephens@gmail.com>
2020-06-29 23:42:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
-# Write your methods here
+def loop_message_five_times(message)
+
+  5.times {puts message}
+  
+end
+
+def loop_message_n_times(message,number)
+  
+number.times {puts message}
+
+end
+
+def output_array(array)
+  array.length {puts array}
+  
+end
+  
+
+  
